35de330b274cb917d2431de1e46869b7e3bb551b
Abdelrahman Omran <me@omranic.com>
2017-08-13 03:20:52 +0200

    Add abilities and policies

---
+++ src/Policies/PagePolicy.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cortex\Pages\Policies;
+
+use Cortex\Fort\Models\User;
+use Cortex\Pages\Models\Page;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PagePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can list pages.
+     *
+     * @param string                   $ability
+     * @param \Cortex\Fort\Models\User $user
+     *
+     * @return bool
+     */
+    public function list($ability, User $user)
+    {
+        return $user->allAbilities->pluck('slug')->contains($ability);
+    }
+
+    /**
+     * Determine whether the user can create pages.
+     *
+     * @param string                   $ability
+     * @param \Cortex\Fort\Models\User $user
+     *
+     * @return bool
+     */
+    public function create($ability, User $user)
+    {
+        return $user->allAbilities->pluck('slug')->contains($ability);
+    }
+
+    /**
+     * Determine whether the user can update the page.
+     *
+     * @param string                                $ability
+     * @param \Cortex\Fort\Models\User              $user
+     * @param \Cortex\Pages\Models\Page $resource
+     *
+     * @return bool
+     */
+    public function update($ability, User $user, Page $resource)
+    {
+        return $user->allAbilities->pluck('slug')->contains($ability);   // User can update pages
+    }
+
+    /**
+     * Determine whether the user can delete the page.
+     *
+     * @param string                                $ability
+     * @param \Cortex\Fort\Models\User              $user
+     * @param \Cortex\Pages\Models\Page $resource
+     *
+     * @return bool
+     */
+    public function delete($ability, User $user, Page $resource)
+    {
+        return $user->allAbilities->pluck('slug')->contains($ability);   // User can delete pages
+    }
+}
