621d56b16cc5ca09a746f6e0c99a9c31ee2bd2e6
Abdelrahman Omran <me@omranic.com>
2017-12-07 19:51:53 +0200

    Add datatable additional hidden columns

--- src/DataTables/Adminarea/PagesDataTable.php
+++ src/DataTables/Adminarea/PagesDataTable.php
@@ -62,9 +62,12 @@ class PagesDataTable extends AbstractDataTable
         return [
             'title' => ['title' => trans('cortex/pages::common.title'), 'render' => $link.'+(full.is_active ? " <i class=\"text-success fa fa-check\"></i>" : " <i class=\"text-danger fa fa-close\"></i>")', 'responsivePriority' => 0],
             'uri' => ['title' => trans('cortex/pages::common.uri')],
+            'domain' => ['title' => trans('cortex/pages::common.domain'), 'visible' => false],
+            'slug' => ['title' => trans('cortex/pages::common.slug'), 'visible' => false],
             'route' => ['title' => trans('cortex/pages::common.route')],
-            'view' => ['title' => trans('cortex/pages::common.view')],
-            'middleware' => ['title' => trans('cortex/pages::common.middleware')],
+            'view' => ['title' => trans('cortex/pages::common.view'), 'visible' => false],
+            'middleware' => ['title' => trans('cortex/pages::common.middleware'), 'visible' => false],
+            'sort_order' => ['title' => trans('cortex/pages::common.sort_order'), 'visible' => false],
             'created_at' => ['title' => trans('cortex/pages::common.created_at'), 'render' => "moment(data).format('MMM Do, YYYY')"],
             'updated_at' => ['title' => trans('cortex/pages::common.updated_at'), 'render' => "moment(data).format('MMM Do, YYYY')"],
         ];
