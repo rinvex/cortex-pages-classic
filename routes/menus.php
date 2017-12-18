215be73dc89593cd8fa83a8b01764e5205853bfa
Abdelrahman Omran <me@omranic.com>
2017-12-18 18:49:44 +0200

    Refactor menus

--- routes/menus.php
+++ routes/menus.php
@@ -2,5 +2,17 @@
 
 declare(strict_types=1);
 
-Menu::adminareaSidebar('resources')->routeIfCan('list-pages', 'adminarea.pages.index', '<i class="fa fa-files-o"></i> <span>'.trans('cortex/pages::common.pages').'</span>');
-Menu::managerareaSidebar('resources')->routeIfCan('list-pages', 'managerarea.pages.index', '<i class="fa fa-files-o"></i> <span>'.trans('cortex/pages::common.pages').'</span>');
+use Rinvex\Menus\Models\MenuItem;
+use Rinvex\Menus\Factories\MenuFactory;
+
+Menu::modify('adminarea.sidebar', function(MenuFactory $menu) {
+    $menu->findBy('title', trans('cortex/foundation::common.cms'), function (MenuItem $dropdown) {
+        $dropdown->route(['adminarea.pages.index'], trans('cortex/pages::common.pages'), 20, 'fa fa-files-o')->can('list-pages');
+    });
+});
+
+Menu::modify('managerarea.sidebar', function(MenuFactory $menu) {
+    $menu->findBy('title', trans('cortex/foundation::common.cms'), function (MenuItem $dropdown) {
+        $dropdown->route(['managerarea.pages.index'], trans('cortex/pages::common.pages'), 20, 'fa fa-files-o')->can('list-pages');
+    });
+});
