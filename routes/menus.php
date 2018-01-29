f1865b0f6a332e7652191e4f01159f7ae1bbd0c1
Abdelrahman Omran <me@omranic.com>
2018-01-29 16:44:29 +0200

    Apply recent menu updates

--- routes/menus.php
+++ routes/menus.php
@@ -3,15 +3,15 @@
 declare(strict_types=1);
 
 use Rinvex\Menus\Models\MenuItem;
-use Rinvex\Menus\Factories\MenuFactory;
+use Rinvex\Menus\Models\MenuGenerator;
 
-Menu::modify('adminarea.sidebar', function (MenuFactory $menu) {
+Menu::register('adminarea.sidebar', function (MenuGenerator $menu) {
     $menu->findByTitleOrAdd(trans('cortex/foundation::common.cms'), 40, 'fa fa-file-text-o', [], function (MenuItem $dropdown) {
         $dropdown->route(['adminarea.pages.index'], trans('cortex/pages::common.pages'), 20, 'fa fa-files-o')->ifCan('list-pages')->activateOnRoute('adminarea.pages');
     });
 });
 
-Menu::modify('managerarea.sidebar', function (MenuFactory $menu) {
+Menu::register('managerarea.sidebar', function (MenuGenerator $menu) {
     $menu->findByTitleOrAdd(trans('cortex/foundation::common.cms'), 40, 'fa fa-file-text-o', [], function (MenuItem $dropdown) {
         $dropdown->route(['managerarea.pages.index'], trans('cortex/pages::common.pages'), 20, 'fa fa-files-o')->ifCan('list-pages')->activateOnRoute('managerarea.pages');
     });
