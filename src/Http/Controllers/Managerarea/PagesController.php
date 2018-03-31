890e04ebb76999e03b477e7883f7eb3e6fb9b752
Abdelrahman Omran <me@omranic.com>
2018-03-31 19:14:24 +0200

    Enforce consistency

--- src/Http/Controllers/Managerarea/PagesController.php
+++ src/Http/Controllers/Managerarea/PagesController.php
@@ -192,7 +192,7 @@ class PagesController extends AuthorizedController
 
         return intend([
             'url' => route('managerarea.pages.index'),
-            'with' => ['success' => trans('cortex/foundation::messages.resource_saved', ['resource' => 'page', 'identifier' => $page->slug])],
+            'with' => ['success' => trans('cortex/foundation::messages.resource_saved', ['resource' => 'page', 'identifier' => $page->name])],
         ]);
     }
 
@@ -209,7 +209,7 @@ class PagesController extends AuthorizedController
 
         return intend([
             'url' => route('managerarea.pages.index'),
-            'with' => ['warning' => trans('cortex/foundation::messages.resource_deleted', ['resource' => 'page', 'identifier' => $page->slug])],
+            'with' => ['warning' => trans('cortex/foundation::messages.resource_deleted', ['resource' => 'page', 'identifier' => $page->name])],
         ]);
     }
 }
