d5efb93a0f3605c72ccb974c71d4a1e3ba5317f5
Abdelrahman Omran <me@omranic.com>
2018-04-06 11:48:56 +0000

    Apply fixes from StyleCI

--- routes/web/managerarea.php
+++ routes/web/managerarea.php
@@ -3,30 +3,29 @@
 declare(strict_types=1);
 
 Route::domain('{subdomain}.'.domain())->group(function () {
-
     Route::name('managerarea.')
          ->namespace('Cortex\Pages\Http\Controllers\Managerarea')
          ->middleware(['web', 'nohttpcache', 'can:access-managerarea'])
          ->prefix(config('cortex.foundation.route.locale_prefix') ? '{locale}/'.config('cortex.foundation.route.prefix.managerarea') : config('cortex.foundation.route.prefix.managerarea'))->group(function () {
 
             // Pages Routes
-            Route::name('pages.')->prefix('pages')->group(function () {
-                Route::get('/')->name('index')->uses('PagesController@index');
-                Route::get('import')->name('import')->uses('PagesController@import');
-                Route::post('import')->name('hoard')->uses('PagesController@hoard');
-                Route::get('import/logs')->name('import.logs')->uses('PagesController@importLogs');
-                Route::get('create')->name('create')->uses('PagesController@create');
-                Route::post('create')->name('store')->uses('PagesController@store');
-                Route::get('{page}')->name('edit')->uses('PagesController@edit');
-                Route::put('{page}')->name('update')->uses('PagesController@update');
-                Route::get('{page}/logs')->name('logs')->uses('PagesController@logs');
-                Route::delete('{page}')->name('destroy')->uses('PagesController@destroy');
+             Route::name('pages.')->prefix('pages')->group(function () {
+                 Route::get('/')->name('index')->uses('PagesController@index');
+                 Route::get('import')->name('import')->uses('PagesController@import');
+                 Route::post('import')->name('hoard')->uses('PagesController@hoard');
+                 Route::get('import/logs')->name('import.logs')->uses('PagesController@importLogs');
+                 Route::get('create')->name('create')->uses('PagesController@create');
+                 Route::post('create')->name('store')->uses('PagesController@store');
+                 Route::get('{page}')->name('edit')->uses('PagesController@edit');
+                 Route::put('{page}')->name('update')->uses('PagesController@update');
+                 Route::get('{page}/logs')->name('logs')->uses('PagesController@logs');
+                 Route::delete('{page}')->name('destroy')->uses('PagesController@destroy');
 
-                Route::name('media.')->prefix('{page}/media')->group(function () {
-                    Route::get('/')->name('index')->uses('PagesMediaController@index');
-                    Route::post('/')->name('store')->uses('PagesMediaController@store');
-                    Route::delete('{media}')->name('destroy')->uses('PagesMediaController@destroy');
-                });
-            });
-        });
+                 Route::name('media.')->prefix('{page}/media')->group(function () {
+                     Route::get('/')->name('index')->uses('PagesMediaController@index');
+                     Route::post('/')->name('store')->uses('PagesMediaController@store');
+                     Route::delete('{media}')->name('destroy')->uses('PagesMediaController@destroy');
+                 });
+             });
+         });
 });
