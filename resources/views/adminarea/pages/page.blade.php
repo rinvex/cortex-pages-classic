86e9b1c85a3b42bb32ff00f257f7e25a946b7a5a
Abdelrahman Omran <me@omranic.com>
2018-07-16 21:08:11 +0200

    Enforce unique element IDs

--- resources/views/adminarea/pages/page.blade.php
+++ resources/views/adminarea/pages/page.blade.php
@@ -122,7 +122,7 @@
                                                     <span class="input-group-btn">
                                                 <span class="btn btn-default btn-file">
                                                     {{ trans('cortex/bookings::common.browse') }}
-                                                    {{ Form::file('profile_picture', ['class' => 'form-control']) }}
+                                                    {{ Form::file('profile_picture', ['class' => 'form-control', 'id' => 'profile_picture_browse']) }}
                                                 </span>
                                             </span>
                                                 </div>
@@ -161,7 +161,7 @@
                                                     <span class="input-group-btn">
                                                 <span class="btn btn-default btn-file">
                                                     {{ trans('cortex/bookings::common.browse') }}
-                                                    {{ Form::file('cover_photo', ['class' => 'form-control']) }}
+                                                    {{ Form::file('cover_photo', ['class' => 'form-control', 'id' => 'cover_photo_browse']) }}
                                                 </span>
                                             </span>
                                                 </div>
