ce19f3f9d986d8c42555eb9b21e13411a9693beb
Abdelrahman Omran <me@omranic.com>
2018-10-24 20:02:10 +0100

    Catch pre-db connection exceptions

--- routes/web/frontarea.php
+++ routes/web/frontarea.php
@@ -4,17 +4,25 @@ declare(strict_types=1);
 
 use Cortex\Pages\Http\Controllers\Frontarea\PagesController;
 
-if (Schema::hasTable(config('rinvex.pages.tables.pages'))) {
-    app('rinvex.pages.page')->where('is_active', true)->get()->groupBy('domain')->each(function ($pages, $domain) {
-        Route::domain($domain ?? domain())->group(function () use ($pages) {
-            $pages->each(function ($page) {
-                Route::get($page->uri)
-                     ->prefix(config('cortex.foundation.route.locale_prefix') ? '{locale}' : '')
-                     ->name($page->route)
-                     ->uses(PagesController::class)
-                     ->middleware($page->middleware ?? ['web'])
-                     ->where('locale', '[a-z]{2}');
+try {
+    // Just check if we have DB connection! This is to avoid
+    // exceptions on new projects before configuring database options
+    DB::connection()->getPdo();
+
+    if (Schema::hasTable(config('rinvex.pages.tables.pages'))) {
+        app('rinvex.pages.page')->where('is_active', true)->get()->groupBy('domain')->each(function ($pages, $domain) {
+            Route::domain($domain ?? domain())->group(function () use ($pages) {
+                $pages->each(function ($page) {
+                    Route::get($page->uri)
+                         ->prefix(config('cortex.foundation.route.locale_prefix') ? '{locale}' : '')
+                         ->name($page->route)
+                         ->uses(PagesController::class)
+                         ->middleware($page->middleware ?? ['web'])
+                         ->where('locale', '[a-z]{2}');
+                });
             });
         });
-    });
+    }
+} catch (Exception $e) {
+    // Be quite! Do not do or say anything!!
 }
