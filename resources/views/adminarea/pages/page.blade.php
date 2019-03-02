6d1ac1f7600be2fc87891349b3ab33950b698c24
Abdelrahman Omran <me@omranic.com>
2019-03-02 17:34:51 +0400

    Add files option to the form to allow file upload

--- resources/views/adminarea/pages/page.blade.php
+++ resources/views/adminarea/pages/page.blade.php
@@ -42,9 +42,9 @@
                     <div class="tab-pane active" id="details-tab">
 
                         @if ($page->exists)
-                            {{ Form::model($page, ['url' => route('adminarea.pages.update', ['page' => $page]), 'method' => 'put', 'id' => "adminarea-pages-{$page->getRouteKey()}-update-form"]) }}
+                            {{ Form::model($page, ['url' => route('adminarea.pages.update', ['page' => $page]), 'method' => 'put', 'id' => "adminarea-pages-{$page->getRouteKey()}-update-form", 'files' => true]) }}
                         @else
-                            {{ Form::model($page, ['url' => route('adminarea.pages.store'), 'id' => 'adminarea-pages-create-form']) }}
+                            {{ Form::model($page, ['url' => route('adminarea.pages.store'), 'id' => 'adminarea-pages-create-form', 'files' => true]) }}
                         @endif
 
                             <div class="row">
