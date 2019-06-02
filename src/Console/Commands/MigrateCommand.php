c303b88d323b6805513d8b6d9e4ee5feca8334a7
Abdelrahman Omran <me@omranic.com>
2019-06-02 05:20:43 +0200

    Refactor migrations and artisan commands, and tweak service provider publishes functionality

--- src/Console/Commands/MigrateCommand.php
+++ src/Console/Commands/MigrateCommand.php
@@ -31,6 +31,16 @@ class MigrateCommand extends BaseMigrateCommand
     {
         parent::handle();
 
-        $this->call('migrate', ['--step' => true, '--path' => 'app/cortex/pages/database/migrations', '--force' => $this->option('force')]);
+        if (file_exists($path = 'database/migrations/cortex/pages')) {
+            $this->call('migrate', [
+                '--step' => true,
+                '--path' => $path,
+                '--force' => $this->option('force'),
+            ]);
+        } else {
+            $this->warn('No migrations found! Consider publish them first: <fg=green>php artisan cortex:publish:pages</>');
+        }
+
+        $this->line('');
     }
 }
