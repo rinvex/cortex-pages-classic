563296f1b348d342f3ca0ccc8fc976e87b91d8ce
Abdelrahman Omran <me@omranic.com>
2020-12-05 12:19:02 +0400

    Type hint Authorizable user parameter

--- routes/channels/adminarea.php
+++ routes/channels/adminarea.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
-Broadcast::channel('adminarea-pages-index', function ($user) {
+use Illuminate\Contracts\Auth\Access\Authorizable;
+
+Broadcast::channel('adminarea-pages-index', function (Authorizable $user) {
     return $user->can('list', app('rinvex.pages.page'));
 }, ['guards' => ['admin']]);
