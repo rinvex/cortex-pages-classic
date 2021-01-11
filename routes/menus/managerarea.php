095c92e9c59b2074e5d90aafab24e075b2698e39
Hendawy <hendaoymaher@yahoo.com>
2021-01-11 13:12:03 +0200

    attach tenants to page in adminarea

--- routes/menus/managerarea.php
+++ routes/menus/managerarea.php
@@ -2,11 +2,10 @@
 
 declare(strict_types=1);
 
-use Cortex\Pages\Models\Page;
 use Rinvex\Menus\Models\MenuItem;
 use Rinvex\Menus\Models\MenuGenerator;
 
-Menu::register('managerarea.sidebar', function (MenuGenerator $menu, Page $page) {
+Menu::register('managerarea.sidebar', function (MenuGenerator $menu) {
     $menu->findByTitleOrAdd(trans('cortex/foundation::common.cms'), 40, 'fa fa-file-text-o', 'header', [], function (MenuItem $dropdown) use ($page) {
         $dropdown->route(['managerarea.cortex.pages.pages.index'], trans('cortex/pages::common.pages'), 20, 'fa fa-files-o')->ifCan('list', $page)->activateOnRoute('managerarea.cortex.pages.pages');
     });
